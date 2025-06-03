fa40f6d2a65232642382602b969c5c316163ecea
tung-ideapad <tung-ideapad@staas.io>
2025-06-03 12:00:37 +0700

    refactor: remove unused evaluation and minimax functions

--- .prompts/temp.js
+++ .prompts/temp.js
@@ -1,36 +1,3 @@
-/*
- * Evaluates the board at this point in time,
- * using the material weights and piece square tables.
- */
-function evaluateBoard(game, move, prevSum, color) {
-}
-
-/*
- * Performs the minimax algorithm to choose the best move: https://en.wikipedia.org/wiki/Minimax (pseudocode provided)
- * Recursively explores all possible moves up to a given depth, and evaluates the game board at the leaves.
- *
- * Basic idea: maximize the minimum value of the position resulting from the opponent's possible following moves.
- * Optimization: alpha-beta pruning: https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning (pseudocode provided)
- *
- * Inputs:
- *  - game:                 the game object.
- *  - depth:                the depth of the recursive tree of all possible moves (i.e. height limit).
- *  - isMaximizingPlayer:   true if the current layer is maximizing, false otherwise.
- *  - sum:                  the sum (evaluation) so far at the current layer.
- *  - color:                the color of the current player.
- *
- * Output:
- *  the best move at the root of the current subtree.
- */
-function minimax(game, depth, alpha, beta, isMaximizingPlayer, sum, color) {
-}
-
-/*
- * Calculates the best legal move for the given color.
- */
-function getBestMove(game, color, currSum) {
-}
-
 // --- Helper functions assumed to be available ---
 
 // Deep clone function: IMPORTANT! This needs to correctly deep clone your entire game state.
